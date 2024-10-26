812a9925a8f493f9ef70a1e4146d08eb062b7e75
Maximus7474 <the.maximus.7474@gmail.com>
2024-10-26 13:49:27 +0200

    rf(ui): handle page selection from NUI

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -9,14 +9,15 @@ function App() {
   const [visible, setVisible] = useState<boolean>(isEnvBrowser());
   const [page, setPage] = useState<string>('identity')
 
-  useNuiEvent('setVisible', (data: { visible?: boolean }) => {
+  useNuiEvent('setVisible', (data: { visible?: boolean, page?: string }) => {
     setVisible(data.visible || false);
+    if (data.page) setPage(data.page);
   });
 
   return (
     <>
       {visible && (
-        <div className="boilerplate-wrapper">
+        <div className="nui-wrapper">
           {page === 'identity' && <Identity />}
           {page === 'multichar' && <Paper style={{padding: 10}}>"MultiChar"</Paper>}
         </div>
