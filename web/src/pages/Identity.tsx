246ff73fe1e276f6e61d73bb48de667d6a97ffe3
Maximus7474 <the.maximus.7474@gmail.com>
2024-10-26 15:33:13 +0200

    feat(ui): Identity confirmation and nui callback emitter

--- web/src/pages/Identity.tsx
+++ web/src/pages/Identity.tsx
@@ -1,17 +1,24 @@
-import { Radio, Paper, Title, Group, TextInput, Button, Flex } from "@mantine/core"
+import { Radio, Paper, Title, Group, TextInput, Button, Flex, Modal, Text } from "@mantine/core"
 import { DateInput } from "@mantine/dates";
 import { useState } from "react";
 
 import styles from './Identity.module.css';
 import '@mantine/dates/styles.css';
+import { fetchNui } from "../utils/fetchNui";
+import { useDisclosure } from "@mantine/hooks";
+import { isEnvBrowser } from "src/utils/misc";
 
 const Identity = () => {
+    const [opened, { open, close }] = useDisclosure(false);
+
     const [firstName, setFirstName] = useState<string>("");
     const [firstNameError, setFirstNameError] = useState<boolean>(false);
     const [lastName, setLastName] = useState<string>("");
     const [lastNameError, setLastNameError] = useState<boolean>(false);
     const [birthday, setBirthday] = useState<Date | null>(null);
     const [gender, setGender] = useState<string | null>(null);
+
+    const [validationResponse, setValidationResponse] = useState<string|null>(null);
 
     const checkName = (name: string) => {
         const regex = /[^a-zA-Z'-\s]/g;
@@ -28,7 +35,26 @@ const Identity = () => {
         )
     };
 
-    return (
+    const validateInformation = () => {
+        close();
+        // const date = birthday?.toLocaleDateString('fr-FR');
+        // const date = birthday?.getTime();
+        const date = birthday;
+        
+        fetchNui('mps-multichar', {
+            firstName, lastName, gender, date
+        }, { data: {status: true, statusMessage: ''}, delay: 200 })
+        .then(r => {
+            if (r.status) {
+                close();
+            } else {
+                setValidationResponse(r.statusMessage);
+                if (isEnvBrowser()) alert(`Character Info Valid: ${r.status}`);
+            }
+        });
+    }
+
+    return (<>
         <Paper shadow="xs" p="xl" style={{
             display: 'flex',
             flexDirection: 'column',
@@ -74,7 +100,7 @@ const Identity = () => {
                 onChange={setBirthday}
                 label="Birthday"
                 placeholder="DD/MM/YYYY"
-                valueFormat="YYYY MMM DD"
+                valueFormat="DD/MM/YYYY"
             />
 
             {/* Gender Selector */}
@@ -116,12 +142,30 @@ const Identity = () => {
                     style={{
                         width: '45%'
                     }}
+                    onClick={open}
                 >
                     Start Character
                 </Button>
             </Flex>
         </Paper>
-    )
+        <Modal opened={opened} onClose={close} title="Confirmation" centered>
+            <Text>Are you sure about the provided information?</Text>
+
+            {
+                typeof validationResponse === 'string' && validationResponse.length > 0 ? 
+                <Flex gap="xs" c="#ea4141">
+                    <Text fw={800} td="underline">Impossible:</Text>
+                    <Text>{validationResponse}</Text>
+                </Flex> :
+                <></>
+            }
+
+            <Flex direction="row" justify="space-evenly" style={{marginTop: '1em'}}>
+                <Button className={styles.resetButton} style={{width: '30%'}} onClick={close}>Cancel</Button>
+                <Button style={{width: '30%'}} onClick={validateInformation}>Submit</Button>
+            </Flex>
+        </Modal>
+    </>)
 }
 
 export default Identity;
